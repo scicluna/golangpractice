546c9248ccafb8720ac9f9141dae951f84327cdc
scicluna <88948700+scicluna@users.noreply.github.com>
2023-08-16 23:17:19 -0400

    chapter 1 clear

--- test.go
+++ test.go
@@ -2,22 +2,49 @@ package main
 
 import "fmt"
 
+//Basic hello world
 // func main() {
 // 	//comments
 // 	fmt.Println("starting Textio server")
 // }
 
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+//:= makes a new variable and assigns it
+//we also can declare the type of variable upfront before the statement
+// func main() {
+// 	messagesFromDoris := []string{
+// 		"You doing anything later??",
+// 		"hey lol",
+// 		"Don't leave me hanging...",
+// 		"Cool",
+// 	}
+// 	numMessages := float64(len(messagesFromDoris))
+// 	costPerMessage := .02
+
+// 	totalCost := costPerMessage * numMessages
+
+// 	fmt.Printf("Doris spent %.2f on text messages today\n", totalCost)
+// }
+
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+//go code generally runs FASTER than interpreted languages and compiles FASTER than other compiled languages like C and Rust
+//does go execute faster than rust? NO
+
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+//main.go -> gobuild -> helloworld.exe
+
+//variable types
 func main() {
-	messagesFromDoris := []string{
-		"You doing anything later??",
-		"hey lol",
-		"Don't leave me hanging...",
-		"Cool",
-	}
-	numMessages := float64(len(messagesFromDoris))
-	costPerMessage := .02
+	//var username string = "wagslane"
+	//var password string = "20947382822"
+	username := "wagslane"
+	password := "20947382822"
 
-	totalCost := costPerMessage * numMessages
-
-	fmt.Printf("Doris spent %.2f on text messages today\n", totalCost)
+	fmt.Println("Authorization: Basic", username+":"+password)
 }
